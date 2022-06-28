ccce1da95739fc3105e63dac913214d46c696fdc
malichun <malichun0926@qq.com>
2022-06-29 01:20:04 +0800

    state part

---
+++ FlinkTutorial/src/main/java/com/atguigu/chapter09_state/Test01State.java
@@ -0,0 +1,124 @@
+package com.atguigu.chapter09_state;
+
+import com.atguigu.chapter05_StreamAPI.ClickSource;
+import com.atguigu.chapter05_StreamAPI.Event;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.common.state.*;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+import java.time.Duration;
+
+/**
+ * 查看ValueState
+ *
+ * @author malichun
+ * @create 2022/6/28 23:56
+ */
+public class Test01State {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        SingleOutputStreamOperator<Event> stream = env.addSource(new ClickSource())
+            .assignTimestampsAndWatermarks(WatermarkStrategy.<Event>forBoundedOutOfOrderness(Duration.ZERO)
+                .withTimestampAssigner(new SerializableTimestampAssigner<Event>() {
+                    @Override
+                    public long extractTimestamp(Event element, long recordTimestamp) {
+                        return element.timestamp;
+                    }
+                }));
+
+        stream.keyBy(data -> data.user)
+            .flatMap(new MyFlatMapFunction())
+            .print();
+
+        env.execute();
+    }
+
+    // 实现自定义的FlatMapFunction, 用于Keyed State测试
+    public static class MyFlatMapFunction extends RichFlatMapFunction<Event, String>{
+
+        // 定义状态
+        ValueState<Event> myValueState;
+        ListState<Event> myListState;
+        MapState<String, Long> myMapState;
+        ReducingState<Event> myReducingState;
+        AggregatingState<Event, String> myAggregateState;
+
+        // 增加一个本地变量进行对比
+        long count;
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            myValueState = getRuntimeContext().getState(new ValueStateDescriptor<Event>("my-state",Event.class));
+            myListState = getRuntimeContext().getListState(new ListStateDescriptor<Event>("my-list-state",Event.class));
+            myMapState = getRuntimeContext().getMapState(new MapStateDescriptor<String, Long>("my-map-state",String.class, Long.class));
+            myReducingState = getRuntimeContext().getReducingState(new ReducingStateDescriptor<Event>("my-reducing-state", new ReduceFunction<Event>() {
+                @Override
+                public Event reduce(Event value1, Event value2) throws Exception {
+                    return new Event(value1.user, value1.url, value2.timestamp);
+                }
+            }, Event.class));
+
+            myAggregateState = getRuntimeContext().getAggregatingState(new AggregatingStateDescriptor<Event, Long, String>("my-agg-state", new AggregateFunction<Event, Long, String>() {
+                @Override
+                public Long createAccumulator() {
+                    return 0L;
+                }
+
+                @Override
+                public Long add(Event value, Long accumulator) {
+                    return accumulator +1;
+                }
+
+                @Override
+                public String getResult(Long accumulator) {
+                    return "count" + accumulator;
+                }
+
+                @Override
+                public Long merge(Long a, Long b) {
+                    return a + b;
+                }
+            }, Long.class));
+        }
+
+        @Override
+        public void flatMap(Event value, Collector<String> out) throws Exception {
+            // 访问和更新状态
+            System.out.println(myValueState.value());
+            myValueState.update(value);
+            //System.out.println("my Value: "+myValueState.value());
+
+            //
+            System.out.println("my list state" + myListState.get());
+
+            myListState.add(value);
+            myMapState.put(value.user, myMapState.get(value.user) == null? 1 :myMapState.get(value.user) + 1);
+            System.out.println("my map value: "+myMapState.get(value.user));
+
+            myAggregateState.add(value);
+            System.out.println("my agg value: "+myAggregateState.get());
+
+            myReducingState.add(value);
+            System.out.println("my reducing state: "+myReducingState.get());
+
+            // 测试本地变量, 不使用状态, 结果: 每来一条任何数据,每来一个会增长一次 +1 (任何key)
+            count++;
+            System.out.println("count: "+ count);
+
+            // 情况状态
+            myValueState.clear();
+        }
+
+
+
+    }
+}
---
+++ FlinkTutorial/src/main/java/com/atguigu/chapter09_state/Test02_MapState_PeriodicPvUv.java
@@ -0,0 +1,88 @@
+package com.atguigu.chapter09_state;
+
+import com.atguigu.chapter05_StreamAPI.ClickSource;
+import com.atguigu.chapter05_StreamAPI.Event;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.util.Collector;
+
+import java.time.Duration;
+
+/**
+ * 我们这里会使用用户 id 来进行分流，然后分别统计每个用户的 pv 数据，由于我们并不想
+ * 每次 pv 加一，就将统计结果发送到下游去，所以这里我们注册了一个定时器，用来隔一段时
+ * 间发送 pv 的统计结果，这样对下游算子的压力不至于太大。具体实现方式是定义一个用来保
+ * 存定时器时间戳的值状态变量。当定时器触发并向下游发送数据以后，便清空储存定时器时间
+ * 戳的状态变量，这样当新的数据到来时，发现并没有定时器存在，就可以注册新的定时器了，
+ * 注册完定时器之后将定时器的时间戳继续保存在状态变量中。
+ * <p>
+ * 周期性生成pv数据
+ *
+ * @author malichun
+ * @create 2022/6/29 0:57
+ */
+public class Test02_MapState_PeriodicPvUv {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        SingleOutputStreamOperator<Event> stream = env.addSource(new ClickSource())
+            .assignTimestampsAndWatermarks(WatermarkStrategy.<Event>forBoundedOutOfOrderness(Duration.ZERO)
+                .withTimestampAssigner(new SerializableTimestampAssigner<Event>() {
+                    @Override
+                    public long extractTimestamp(Event element, long recordTimestamp) {
+                        return element.timestamp;
+                    }
+                }));
+
+        stream.print();
+
+        // 统计每个用户的pv
+        stream.keyBy(data -> data.user)
+            .process(new PeriodicPvResult())
+            .print();
+
+        env.execute();
+    }
+
+    // 实现自定义的KeyedProcessFunction
+    public static class PeriodicPvResult extends KeyedProcessFunction<String, Event, String>{
+
+        // 定义状态, 保存当前pv统计值, 以及有没有定时器
+        ValueState<Long> countState;
+        // 定时器的状态
+        ValueState<Long> timerTsState;
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            countState = getRuntimeContext().getState(new ValueStateDescriptor<Long>("count", Long.class));
+            timerTsState = getRuntimeContext().getState(new ValueStateDescriptor<Long>("timerTs", Long.class));
+        }
+
+        @Override
+        public void processElement(Event value, KeyedProcessFunction<String, Event, String>.Context ctx, Collector<String> out) throws Exception {
+            // 没来一条数据, 就更新对应的count值
+            countState.update(countState.value() == null ? 1 : countState.value()+1);
+
+            // 如果没有注册过的话, 注册定时器
+            if(timerTsState.value() == null){
+                timerTsState.update(value.timestamp);
+                ctx.timerService().registerEventTimeTimer(value.timestamp + 10000L);
+            }
+        }
+
+        @Override
+        public void onTimer(long timestamp, KeyedProcessFunction<String, Event, String>.OnTimerContext ctx, Collector<String> out) throws Exception {
+            // 定时器触发, 输出一次统计结果
+            out.collect(ctx.getCurrentKey() + " pv: " + (countState.value() == null? 0: countState.value()));
+            // 时间状态清空
+            timerTsState.clear();
+        }
+    }
+}
