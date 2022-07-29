d8c25c07460cd5d34a937c148528a86a648023c1
malc <malc@253.com>
2022-07-29 11:57:12 +0800

    添加flink1.14æ模块

---
+++ FlinkTutorial1.14/src/main/java/cn/doitedu/chapter02_baseapi/WindowWordCount.java
@@ -0,0 +1,42 @@
+package cn.doitedu.chapter02_baseapi;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+import java.util.Arrays;
+
+/**
+ * @author malc
+ * @create 2022/7/29 0029 11:41
+ */
+public class WindowWordCount {
+    public static void main(String[] args) throws Exception{
+        Configuration configuration = new Configuration();
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(configuration);
+
+        SingleOutputStreamOperator<Tuple2<String, Integer>> dataStream = env.socketTextStream("hadoop01", 6666)
+            .flatMap(new Splitter())
+            .keyBy(value -> value.f0)
+            .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
+            .sum(1);
+
+        dataStream.print();
+
+        env.execute("window WordCount");
+
+    }
+
+    public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>>{
+
+        @Override
+        public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception {
+            Arrays.stream(s.split(" ")).forEach(word -> collector.collect(Tuple2.of(word,1)));
+        }
+    }
+}
